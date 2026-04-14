9c706020a34ba3433f2b31d3f8600361e0716447
Mohammed Abrar <mame.534424@gmail.com>
2026-04-14 10:21:56 +0300

    feat: replace ScrollView with FlatList for improved performance and item rendering

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import { useAuth, useUser } from "@clerk/clerk-expo";
 import { router } from "expo-router";
-import { ScrollView } from "react-native";
+import { FlatList, ScrollView } from "react-native";
 import { Text, TouchableOpacity, View } from "react-native";
 import TabScreenBackground from "../components/TabScreenBackground";
 import HeroScreenLayout from "../components/list/HeroScreenLayout";
@@ -12,7 +12,33 @@ export default function ListItemPage() {
   const {items}=useGroceryStore();
   const pendingitems=items.filter(item=>!item.purchased);
   return (
-    <ScrollView
+    <FlatList
+    className="flex-1 bg-background"
+    data={pendingitems}
+    renderItem={({ item }) => <PendingItemCard item={item} />}
+    keyExtractor={(item) => item.id}
+    contentContainerStyle={{ padding: 20, gap: 14 }}
+    contentInsetAdjustmentBehavior="automatic"
+    ListHeaderComponent={
+      <View style={{ gap: 14, paddingTop: 20 }}>
+        <TabScreenBackground />
+        <HeroScreenLayout />
+        <View className="flex-row items-center justify-between px-1">
+            <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
+              Shopping items
+            </Text>
+            <Text className="text-sm text-muted-foreground">{pendingitems.length} active</Text>
+        </View>
+        
+
+      </View>
+      }
+    ListFooterComponent={<CompletedItemsSection />} 
+   />
+  );
+}
+
+{/* <ScrollView
       className="bg-background flex-1 py-4"
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{ padding: 20, gap: 14 }}>
@@ -31,6 +57,4 @@ export default function ListItemPage() {
         <PendingItemCard key={item.id} item={item} />
       ))}
       <CompletedItemsSection />
-    </ScrollView>
-  );
-}
+    </ScrollView> */}
