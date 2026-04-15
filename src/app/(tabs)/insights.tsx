ccb0e9c638c1f65ab58323ad4fd549bda4ecf7d3
Mohammed Abrar <mame.534424@gmail.com>
2026-04-15 10:55:50 +0300

    feat: integrate Sentry for error tracking and enhance insights screen with user profile and statistics

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,13 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import TabScreenBackground from '../components/TabScreenBackground'
+import UserProfileCard from '../components/insight/UserProfileCard'
+import InsightStatsSection from '../components/insight/InsightStatsSection'
+import InsightCategorySection from '../components/insight/InsightCategorySection'
+import InsightsPrioritySection from '../components/insight/InsightsPrioritySection'
+import ClearCompletedButton from '../components/insight/ClearCompletedButton'
 
 const insightsScreen = () => {
   return (
-    <View>
-      <Text
-      className='text-white'
-      >insights</Text>
-    </View>
+    <ScrollView
+      className="bg-background flex-1 py-4"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 20, gap: 14 }}
+      contentInsetAdjustmentBehavior="automatic">
+        <TabScreenBackground/>
+
+        <UserProfileCard/>
+
+        
+        <InsightCategorySection/>
+        <InsightStatsSection/>
+        <InsightsPrioritySection/>
+        <ClearCompletedButton />
+      </ScrollView>
   )
 }
 
